b23552c005b52200df3264eed319aa8987909802
mariofpalos <marioferpa@gmail.com>
2022-12-01 10:38:22 +0100

    There's a square in the middle now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+        .add_startup_system(setup_system)
+        .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::RegularPolygon {
+        sides: 4,
+        feature: shapes::RegularPolygonFeature::Radius(50.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(GeometryBuilder::build_as(
+        &shape,
+        DrawMode::Outlined {
+            fill_mode: FillMode::color(Color::GRAY),
+            outline_mode: StrokeMode::new(Color::BLACK, 1.0),
+        },
+        Transform::default(),
+    ));
 }
