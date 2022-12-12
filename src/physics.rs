214f88f5d41d15de8701998b5bd51a22dcf6ce75
mariofpalos <marioferpa@gmail.com>
2022-12-12 17:59:29 +0100

    Added option to increase iterations, and it somehow made it worse!

--- src/physics.rs
+++ src/physics.rs
@@ -9,6 +9,7 @@ use crate::{ components, resources };
 pub const ACCELERATION_X:   f32 = 0.0;    // pixel*s⁻² ?
 pub const ACCELERATION_Y:   f32 = -9.8;   // pixel*s⁻² ?
 pub const PHYSICS_TIMESTEP: f32 = 1.0/60.0; // seconds
+const ITERATIONS:           i32 = 1;
 
 pub struct PhysicsPlugin;
 
@@ -47,59 +48,61 @@ impl PhysicsPlugin {
         // Simulation loop, for however many timesteps are needed
         for _ in 0..timesteps { // Make sure that this is not skipping one or something
 
+            for _ in 0..ITERATIONS {
             // Iterating over esail elements, in order. The first one is skipped.
-            for (index, sail_element) in esail.elements.iter().enumerate().skip(1) {
+                for (index, sail_element) in esail.elements.iter().enumerate().skip(1) {
 
-                // Getting coordinates of the previous sail element in line
-                let prev_sail_element = esail.elements[index - 1];
+                    // Getting coordinates of the previous sail element in line
+                    let prev_sail_element = esail.elements[index - 1];
 
-                let (_prev_element, transform, _can_move) = sail_query.get(prev_sail_element).expect("No previous sail element found");
-                //println!("{}", transform.translation.y);
+                    let (_prev_element, transform, _can_move) = sail_query.get(prev_sail_element).expect("No previous sail element found");
+                    //println!("{}", transform.translation.y);
 
-                let prev_element_x = transform.translation.x;
-                let prev_element_y = transform.translation.y;
+                    let prev_element_x = transform.translation.x;
+                    let prev_element_y = transform.translation.y;
 
-                // Getting coordinates of the current element and modifying its state
-                let (_element, mut transform, mut can_move) = sail_query.get_mut(*sail_element).expect("No sail element found");
+                    // Getting coordinates of the current element and modifying its state
+                    let (_element, mut transform, mut can_move) = sail_query.get_mut(*sail_element).expect("No sail element found");
 
-                // APPLYING (gravitational for now) ACCELERATION
+                    // APPLYING (gravitational for now) ACCELERATION
 
-                let position_x = transform.translation.x;
-                let position_y = transform.translation.y;
+                    let position_x = transform.translation.x;
+                    let position_y = transform.translation.y;
 
-                let velocity_x = position_x - can_move.previous_x;
-                let velocity_y = position_y - can_move.previous_y;
+                    let velocity_x = position_x - can_move.previous_x;
+                    let velocity_y = position_y - can_move.previous_y;
 
-                let next_position_x = position_x + velocity_x + ACCELERATION_X * sim_params.timestep * sim_params.timestep;
-                let next_position_y = position_y + velocity_y + ACCELERATION_Y * sim_params.timestep * sim_params.timestep;
+                    let next_position_x = position_x + velocity_x + ACCELERATION_X * sim_params.timestep * sim_params.timestep;
+                    let next_position_y = position_y + velocity_y + ACCELERATION_Y * sim_params.timestep * sim_params.timestep;
 
-                // APPLYING CONSTRAINTS
+                    // APPLYING CONSTRAINTS
 
-                // Calculating distance between current sail element and previous element in the line
-                let diff_x = position_x - prev_element_x;
-                let diff_y = position_y - prev_element_y;
-                let distance_between_elements = (diff_x * diff_x + diff_y * diff_y).sqrt();
-                
-                let mut difference = 0.0;
+                    // Calculating distance between current sail element and previous element in the line
+                    let diff_x = position_x - prev_element_x;
+                    let diff_y = position_y - prev_element_y;
+                    let distance_between_elements = (diff_x * diff_x + diff_y * diff_y).sqrt();
+                    
+                    let mut difference = 0.0;
 
-                if distance_between_elements > 0.0 {
-                    // Don't get this
-                    //difference = (esail.resting_distance - distance_between_elements) / distance_between_elements;
-                    difference = (distance_between_elements - esail.resting_distance) / distance_between_elements;
+                    if distance_between_elements > 0.0 {
+                        // Don't get this
+                        //difference = (esail.resting_distance - distance_between_elements) / distance_between_elements;
+                        difference = (distance_between_elements - esail.resting_distance) / distance_between_elements;
+                    }
+
+                    let translate_x = diff_x * difference;
+                    let translate_y = diff_y * difference;
+
+                    // APPLYING INERTIA
+
+                    // Updating positions
+
+                    can_move.previous_x = transform.translation.x;
+                    can_move.previous_y = transform.translation.y;
+
+                    transform.translation.x = next_position_x - translate_x;
+                    transform.translation.y = next_position_y - translate_y;
                 }
-
-                let translate_x = diff_x * difference;
-                let translate_y = diff_y * difference;
-
-                // APPLYING INERTIA
-
-                // Updating positions
-
-                can_move.previous_x = transform.translation.x;
-                can_move.previous_y = transform.translation.y;
-
-                transform.translation.x = next_position_x - translate_x;
-                transform.translation.y = next_position_y - translate_y;
             }
         }
     }
