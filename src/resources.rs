003f5921e64abc543a5ecc786094b1110d71e627
mariofpalos <marioferpa@gmail.com>
2023-01-31 12:15:47 +0100

    Added a direction to the solar wind

--- src/resources.rs
+++ src/resources.rs
@@ -82,7 +82,8 @@ impl SpacecraftParameters {
 pub struct SolarWindParameters {
     pub n_0:        quantities::VolumetricNumberDensity,    // Undisturbed solar wind electron density
     pub velocity:   quantities::Velocity, 
-    pub T_e:        quantities::Energy,  // Solar wind electron temperature at 1AU
+    pub direction:  Vec<f64>,
+    pub T_e:        quantities::Energy,                     // Solar wind electron temperature
 }
 
 impl Default for SolarWindParameters {
@@ -90,7 +91,8 @@ impl Default for SolarWindParameters {
         SolarWindParameters {
             n_0:        quantities::VolumetricNumberDensity::new::<volumetric_number_density::per_cubic_centimeter>(7.3),
             velocity:   quantities::Velocity::new::<velocity::meter_per_second>(4.0e5), //(from google, can't find it in the paper)
-            T_e:        quantities::Energy::new::<energy::electronvolt>(12.0),
+            direction:  vec![-1.0, 0.0, 0.0],   // It should be a unit vector, right?
+            T_e:        quantities::Energy::new::<energy::electronvolt>(12.0),          // Solar wind electron temperature at 1AU
         }
     }
 }
