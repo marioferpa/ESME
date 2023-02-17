45767f96076ce67f07bfe0ed98e23d222c924a8d
mariofpalos <marioferpa@gmail.com>
2023-02-17 12:51:15 +0100

    Verlet transform is now updated in graphics.rs

--- src/graphics.rs
+++ src/graphics.rs
@@ -8,6 +8,7 @@ impl Plugin for GraphicsPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_system(Self::gizmo_visibility)
+            .add_system(Self::update_transform_verlets)     // Updates the position of the graphics
             ;
     }
 }
@@ -30,4 +31,19 @@ impl GraphicsPlugin {
         com_entity.is_visible = simulation_parameters.com_visibility;
         axes_entity.is_visible = simulation_parameters.axes_visibility;
     }
+
+    /// Updates the transform of the verlet objects after the simulation, so that the graphics get updated.
+    /// Maybe it will need to update graphics of other things as well.
+    fn update_transform_verlets(
+        mut verlet_query: Query<(&components::VerletObject, &mut Transform)>,
+        ){
+        
+        for (verlet_object, mut transform) in verlet_query.iter_mut() {
+            transform.translation.x = verlet_object.current_x as f32;
+            transform.translation.y = verlet_object.current_y as f32;
+            transform.translation.z = verlet_object.current_z as f32;
+        }
+
+        // Should this update the rotation of the segments too?
+    } 
 }
