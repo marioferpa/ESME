fa4005392d9b9a48741d5f40b84cae27b9ffdea2
Mario F. Palos <marioferpa@gmail.com>
2024-02-26 16:07:20 +0100

    Just formatting and stuff

--- src/lights_and_camera.rs
+++ src/lights_and_camera.rs
@@ -4,7 +4,7 @@ use bevy::window::PrimaryWindow;
 use crate::{ components };
 
 
-// -------------------------- Camera plugin ---------------------------------------
+// -------------------------- Camera plugin ------------------------------------
 
 pub struct LightsAndCameraPlugin;
 
@@ -13,14 +13,18 @@ impl Plugin for LightsAndCameraPlugin {
         app
             .add_systems(
                 Startup, (
-                    Self::spawn_camera,
-                    Self::spawn_light,
+                    spawn_camera,
+                    spawn_light,
                 )
             );
     }
 }
 
-//pub fn get_primary_window_size(windows: &Res<Windows>) -> Vec2 {
+
+
+// Functions -------------------------------------------------------------------
+
+
 pub fn get_primary_window_size (
     window_query: &Query<&Window, With<PrimaryWindow>>
 ) -> Vec2 {
@@ -32,28 +36,27 @@ pub fn get_primary_window_size (
     return window_size;
 }
 
-// TODO Take these out of the impl block
-impl LightsAndCameraPlugin {
 
-    fn spawn_camera(
-        mut commands: Commands,
-        ) {
 
-        let translation = Vec3::new(-500.0, 100.0, 500.0);
-        let radius = translation.length();
+fn spawn_camera(
+    mut commands: Commands,
+) {
 
-        // 3D camera 
-        commands.spawn((
-            Camera3dBundle { transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y), ..default() },
-            components::PanOrbitCamera { radius, ..Default::default() },
-            ));
-        
-    }
+    let translation = Vec3::new(-500.0, 100.0, 500.0);
+    let radius = translation.length();
 
-    fn spawn_light( mut commands: Commands) {
-        commands.spawn(DirectionalLightBundle {
-            ..default()
-        });
-    }
+    // 3D camera 
+    commands.spawn((
+        Camera3dBundle { transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y), ..default() },
+        components::PanOrbitCamera { radius, ..Default::default() },
+        ));
+    
 }
 
+
+
+fn spawn_light( mut commands: Commands) {
+    commands.spawn(DirectionalLightBundle {
+        ..default()
+    });
+}
