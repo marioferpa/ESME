c350ff4bc2206ef1ac94099768bc0cb925fd6199
Mario F. Palos <marioferpa@gmail.com>
2024-03-04 14:14:10 +0100

    Text wall explaining what I should do next

--- src/graphics/new_esail.rs
+++ src/graphics/new_esail.rs
@@ -4,6 +4,21 @@ use uom::si::length::meter;
 
 use crate::{ resources, spacecraft };
 
+// I think I see the problem that past me was trying to solve. I have a vector
+// of VerletObject, and a sphere needs to be spawned and moved for each Verlet.
+// Problem is that if I just update this function and put a sphere where it
+// belongs, I would be spawning new spheres constantly instead of moving them. I
+// would of course prefer to move them, but then how do I track which sphere
+// should go where?
+
+// If I keep the spheres in a vector, as I was doing, and I know that this
+// vector is as long as the deployed verlets, I can be sure that each index
+// corresponds to the sphere of the same index. Does that work when reeling in
+// and out though?
+//
+// It should be the same I guess. If I add an item to the start of the esail and
+// do the same thing to the spheres then the index should keep working
+
 // This is drawing once, spawning the spheres. I guess I am missing a function
 // that updates it
 pub (super) fn draw_new_esail (
@@ -32,7 +47,16 @@ pub (super) fn draw_new_esail (
             commands.spawn ( 
 
                 PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::UVSphere { radius: sphere_radius, ..default() })),
+
+                    mesh: meshes.add(
+                        Mesh::from(
+                            shape::UVSphere { 
+                                radius: sphere_radius, 
+                                ..default() 
+                            }
+                        )
+                    ),
+
                     material: materials.add(
                         StandardMaterial {
                             base_color: Color::rgb(1.0, 0.0, 0.0),
@@ -40,6 +64,7 @@ pub (super) fn draw_new_esail (
                         }
                         .into(),
                     ),
+
                     transform: Transform::from_xyz(
                         verlet_object.current_coordinates.x().get::<meter>() as f32 * 
                             simulation_parameters.pixels_per_meter as f32, 
@@ -48,7 +73,6 @@ pub (super) fn draw_new_esail (
                     ),
                     ..default()
                 }
-
             ).id();
 
         // TODO: What the hell is this?
